b0c51f2cd0d88bd04afe59fb0e24cac25812d558
mostafa mahmoud <mostafa.mahmoudegy10@gmail.com>
2025-02-12 04:09:15 +0200

    for4

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ const App = () => {
       <div className="navbar bg-base-100 shadow-sm">
         <a className="btn btn-ghost text-xl">daisyUI</a>
       </div>
+      <div>
+        
+      </div>
     </div>
   )
 }
